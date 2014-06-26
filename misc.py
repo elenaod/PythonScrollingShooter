bdf85627e6f607b6b885ea9692c4b48f59b1b0ee
elenaod <elenaod@users.noreply.github.com>
2014-06-27 02:40:39 +0300

    misc functions used in the game

---
+++ misc.py
@@ -0,0 +1,28 @@
+from os import system
+
+# define some constants
+SIZE = 10
+MAX_HEALTH = 10
+TIMER = 5
+
+def outside_of_board(x, y):
+    return x < 0 or y < 0 or x >= SIZE or y >= SIZE
+
+def valid(x, y, board):
+    return not outside_of_board(x, y) and board.rows[x][y] == None           
+
+def get_command_console():
+    command = input(">:")
+    command = command[0]
+    if command == 'a':
+        return "move_player_left"
+    elif command == 'd':
+        return "move_player_right"
+    elif command == 'w':
+        return "move_player_up"
+    elif command == 'x':
+        return "move_player_down"
+    if command == 's':
+        return "player_shoot"
+    if command == 'q':
+        return "game_stop"
